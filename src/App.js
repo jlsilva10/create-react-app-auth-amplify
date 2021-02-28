6c760629208ba1f78e15ffa429b304f9bd42d1ca
jsilva <jsilva@tillster.com>
2021-02-28 22:51:00 +0000

    Commit Simple Page

--- src/App.js
+++ src/App.js
@@ -8,9 +8,27 @@ Amplify.configure(aws_exports);
 class App extends Component {
   render() {
     return (
+      <div>
       <div className="App">
-      <h1>2 Truths and a Lie</h1>
-      <h2>Icebreaker Game</h2>
+        <h1>2 Truths and a Lie</h1>
+        <h2>Icebreaker Game</h2>
+      </div>
+      <div className="Selection">
+        <h1>Likes/Dislikes</h1>
+        <form action="/action_page.php">
+  <p>Your guess:</p>
+  <input type="radio" id="male" name="gender" value="male" />
+  <label for="male">I like broccoli</label>
+  <br/>
+  <input type="radio" id="female" name="gender" value="female" />
+  <label for="female">I am a vegetarian</label>
+  <br/>
+  <input type="radio" id="other" name="gender" value="other" />
+  <label for="other">I like turtles</label>
+  <br/>
+  <input type="submit" value="Submit" />
+        </form>
+      </div>
     </div>
     );
   }
