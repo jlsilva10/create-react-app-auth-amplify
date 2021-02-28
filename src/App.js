c76dcf5c6a756fb510b2283dd4bc1d9d79eef31a
jsilva <jsilva@tillster.com>
2021-02-28 17:17:13 +0000

    Commit test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-        <p className="p1"><span className="Apple-converted-space">    </span>&lt;&gt;HOME&lt;/a&gt;</p>
+          <p>HOME</p>
         </header>
       </div>
     );
