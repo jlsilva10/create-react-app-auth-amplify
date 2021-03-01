065b5ddbf45992c28060f07df57ace80d14158c6
jsilva <jsilva@tillster.com>
2021-03-01 00:25:44 +0000

    Commit Simple Page

--- src/App.js
+++ src/App.js
@@ -8,25 +8,31 @@ Amplify.configure(aws_exports);
 class App extends Component {
   render() {
     return (
+      <div className="All">
       <div>
-      <div className="Title">
-        <h1>2 Truths and a Lie</h1>
-        <h2>Icebreaker Game</h2>
+        <h1 className="Title">2 Truths and a Lie</h1>
+        <h2 className="Subtitle">Icebreaker Game</h2>
       </div>
       <div className="Selection">
-        <h1>Likes/Dislikes</h1>
+        <h1 className="TabTitle">Likes/Dislikes</h1>
         <form action="/action_page.php">
-  <p>Your guess:</p>
-  <input type="radio" id="male" name="gender" value="male" />
-  <label for="male">I like broccoli</label>
-  <br/>
-  <input type="radio" id="female" name="gender" value="female" />
-  <label for="female">I am a vegetarian</label>
-  <br/>
-  <input type="radio" id="other" name="gender" value="other" />
-  <label for="other">I like turtles</label>
-  <br/>
-  <input type="submit" value="Submit" />
+          <p className="TabTitle">Your guess:</p>
+          <input type="radio" id="male" name="gender" value="male" />
+          <label for="male" className="TabTitle">
+            I like broccoli
+          </label>
+          <br />
+          <input type="radio" id="female" name="gender" value="female" />
+          <label for="female" className="TabTitle">
+            I am a vegetarian
+          </label>
+          <br />
+          <input type="radio" id="other" name="gender" value="other" />
+          <label for="other" className="TabTitle">
+            I like turtles
+          </label>
+          <br />
+          <input type="submit" value="Submit" />
         </form>
       </div>
     </div>
